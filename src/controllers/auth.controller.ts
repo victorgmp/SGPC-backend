3ab667097dd97e568a5785583677e6831cd1b667
Víctor Gabriel Moreno <victorgmp@gmail.com>
2021-06-30 17:06:36 -0400

    Dev (#2)
    * refactor
    
    * test all auth routes, update auth controller and service
    
    * update request

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { IUser } from '../interfaces';
 
 import { IUserModel } from '../models/user.model';
-import * as userService from '../services/user.services';
+import * as userService from '../services/auth.services';
 
 // helper functions
 const setTokenCookie = (res: Response, token: string) => {
@@ -21,17 +21,17 @@ export const verifyEmail = async (req: Request, res: Response)
       return res.status(400).json({ status: false, message: 'No token received' });
     }
 
-    const isVerified: boolean = await userService.verifyEmail(req.body.token);
-    if (isVerified) {
-      return res.status(200).json({ status: true, message: 'Verification successful, you can now login' });
-    }
-    return res.status(400).json({ status: false, message: 'Verification failed' });
+    await userService.verifyEmail(req.body.token);
+    return res.status(200).json({ status: true, message: 'Verification successful, you can now login' });
   } catch (error) {
-    console.log('Error verifying email:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'EmailVerificationError':
+        return res.status(400).send('Verification failed');
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
-//
 
 export const signUp = async (req: Request, res: Response)
 : Promise<Response<any, Record<string, any>>> => {
@@ -50,15 +50,16 @@ export const signUp = async (req: Request, res: Response)
     }
 
     const user: IUserModel = req.body;
-    const newUser: boolean = await userService.signUp(user, req.get('origin'));
-    if (newUser) {
-      return res.status(201).send({ status: true, message: 'Registration successful, please check your email for verification instructions' });
-    }
+    await userService.signUp(user, req.get('origin'));
 
-    return res.status(400).json({ status: false, message: 'Registration failed' });
+    return res.status(201).send({ status: true, message: 'Registration successful, please check your email for verification instructions' });
   } catch (error) {
-    console.log('Error signup user:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'SignUpError':
+        return res.status(400).json({ status: false, message: 'Registration failed' });
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -81,8 +82,12 @@ export const signIn = async (req: Request, res: Response)
 
     return res.status(200).json({ status: true, payload: userData });
   } catch (error) {
-    console.log('Error signup user:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'SignInError':
+        return res.status(400).json({ status: false, message: 'Email or password wrong' });
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -96,8 +101,12 @@ export const forgotPassword = async (req: Request, res: Response)
     await userService.forgotPassword(req.body.email, req.get('origin'));
     return res.status(200).json({ status: true, message: 'Please check your email for password reset instructions' });
   } catch (error) {
-    console.log('Error recovering password:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'ForgotPasswordError':
+        return res.status(400).json({ status: false, message: 'Error recovering password' });
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -108,15 +117,16 @@ export const resetPassword = async (req: Request, res: Response)
       return res.status(400).json({ status: false, message: 'Please send your new password' });
     }
 
-    const isReset: boolean = await userService.resetPassword(req.body.token, req.body.password);
-    if (isReset) {
-      return res.status(200).json({ status: true, message: 'Password reset successful, you can now login' });
-    }
+    await userService.resetPassword(req.body.token, req.body.password);
 
-    return res.status(400).json({ status: false, message: 'Invalid token' });
+    return res.status(200).json({ status: true, message: 'Password reset successful, you can now login' });
   } catch (error) {
-    console.log('Error recovering password:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'ResetPasswordError':
+        return res.status(400).json({ status: false, message: 'Error resetting password' });
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -127,15 +137,15 @@ export const validateResetToken = async (req: Request, res: Response)
       return res.status(400).json({ status: false, message: 'Please send your token' });
     }
 
-    const isValid: boolean = await userService.validateResetToken(req.body.token);
-    if (isValid) {
-      return res.status(200).json({ status: true, message: 'Token is valid' });
-    }
-
-    return res.status(400).json({ status: false, message: 'Invalid token' });
+    await userService.validateResetToken(req.body.token);
+    return res.status(200).json({ status: true, message: 'Token is valid' });
   } catch (error) {
-    console.log('Error validating reset token:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'ValidateResetTokenError':
+        return res.status(400).json({ status: false, message: 'Invalid token' });
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -156,8 +166,12 @@ export const refreshToken = async (req: Request, res: Response)
 
     return res.status(200).json({ status: true, payload: userData });
   } catch (error) {
-    console.log('Error refreshing token:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'RefreshTokenError':
+        return res.status(401).send('Unauthorized');
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
 
@@ -175,12 +189,15 @@ export const revokeToken = async (req: Request, res: Response)
       return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const isRevoked = await userService.revokeToken(token, ipAddress);
-    if (!isRevoked) return res.status(401).json({ message: 'Unauthorized' });
+    await userService.revokeToken(token, ipAddress);
 
     return res.status(200).json({ status: true, message: 'Token revoked' });
   } catch (error) {
-    console.log('Error revoking token:', error);
-    return res.status(500).send('Internal Server Error');
+    switch (error.message) {
+      case 'RevokeTokenError':
+        return res.status(401).send('Unauthorized');
+      default:
+        return res.status(500).send('Internal Server Error');
+    }
   }
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -52,13 +52,4 @@ userSchema.virtual('isVerified').get(function (this: { verified: string, passwor
   return !!(this.verified || this.passwordReset);
 });
 
-// userSchema.methods.encryptPassword = async (password: string) => {
-//   const salt = await bcrypt.genSalt(10);
-//   return await bcrypt.hash(password, salt);
-// };
-
-// userSchema.methods.comparePassword = async (password: string, receivedPassword: string) => {
-//   return await bcrypt.compare(password, receivedPassword);
-// };
-
 export default model<IUserModel>('user', userSchema);
